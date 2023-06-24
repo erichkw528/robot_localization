70d3cc845ed533bf7cdeb18c5f069ce05680cb71
Michael Wu <wuxiaohua1011@gmail.com>
2023-06-23 23:51:06 -0700

    lite: done

--- include/roar_robot_localization/odom_publisher.hpp
+++ include/roar_robot_localization/odom_publisher.hpp
@@ -10,15 +10,24 @@
 #include <sensor_msgs/msg/imu.hpp>
 #include <nav_msgs/msg/odometry.hpp>
 #include <tf2_ros/transform_broadcaster.h>
+#include <iomanip>  // Include the <iomanip> header for setprecision
+#include <sstream>
 
 namespace roar
 {
 
 struct BufferData
 {
-  sensor_msgs::msg::Imu::ConstSharedPtr img_msg;
+  sensor_msgs::msg::Imu::ConstSharedPtr imu_msg;
   sensor_msgs::msg::NavSatFix::ConstSharedPtr gps_msg;
   geometry_msgs::msg::PoseStamped::ConstSharedPtr pose_msg;
+};
+
+struct GpsPosition
+{
+  double latitude;
+  double longitude;
+  double altitude;
 };
 
 typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Imu, sensor_msgs::msg::NavSatFix,
@@ -52,6 +61,41 @@ protected:
   // data
   std::vector<BufferData> buffer;
   int bufferSize = 10;
+  GpsPosition map_origin_;
+
+  /**
+   * Convert data using GeographicLib
+   * using imu + pose to estimate linear speed
+   * using imu to get orientation and angular
+   */
+  void p_calcOdom(const std::vector<BufferData>* buffer, nav_msgs::msg::Odometry::SharedPtr odom);
+
+  std::string to_string(const nav_msgs::msg::Odometry& odom)
+  {
+    std::ostringstream oss;
+    oss << "  Stamp: " << odom.header.stamp.sec << "." << std::setfill('0') << std::setw(9) << odom.header.stamp.nanosec
+        << std::endl;
+    oss << "  Position:";
+    oss << "    x: " << odom.pose.pose.position.x;
+    oss << "    y: " << odom.pose.pose.position.y;
+    oss << "    z: " << odom.pose.pose.position.z;
+    oss << "" << std::endl;
+    oss << "  Orientation:";
+    oss << "    x: " << odom.pose.pose.orientation.x;
+    oss << "    y: " << odom.pose.pose.orientation.y;
+    oss << "    z: " << odom.pose.pose.orientation.z;
+    oss << "    w: " << odom.pose.pose.orientation.w << std::endl;
+    oss << "Twist  Linear:";
+    oss << "    x: " << odom.twist.twist.linear.x;
+    oss << "    y: " << odom.twist.twist.linear.y;
+    oss << "    z: " << odom.twist.twist.linear.z << std::endl;
+    oss << "Twist  Angular:";
+    oss << "    x: " << odom.twist.twist.angular.x;
+    oss << "    y: " << odom.twist.twist.angular.y;
+    oss << "    z: " << odom.twist.twist.angular.z << std::endl;
+    // oss << "" << std::endl;
+    return oss.str();
+  }
 };
 
 }  // namespace roar
--- src/odom_publisher.cpp
+++ src/odom_publisher.cpp
@@ -2,6 +2,11 @@
 #include <rmw/qos_profiles.h>
 #include "rmw/types.h"
 #include <geometry_msgs/msg/transform_stamped.hpp>
+#include "GeographicLib/Geocentric.hpp"
+#include "GeographicLib/LocalCartesian.hpp"
+
+#include <sstream>
+
 namespace roar
 {
 
@@ -10,7 +15,17 @@ OdomPublisher::OdomPublisher() : Node("odom_publisher_node")
   // variables
   this->declare_parameter("odom_frame", "odom");
   this->declare_parameter("base_link_frame", "base_link");
-  this->declare_parameter("buffer_size", 10);
+  this->declare_parameter("buffer_size", 5);
+  this->declare_parameter("rate_millis", 50);
+  this->declare_parameter("debug", false);
+  if (this->get_parameter("debug").as_bool())
+  {
+    auto ret = rcutils_logging_set_logger_level(get_logger().get_name(),
+                                                RCUTILS_LOG_SEVERITY_DEBUG);  // enable or disable debug
+  }
+
+  map_origin_ = { declare_parameter("map_origin.latitude", 0.0), declare_parameter("map_origin.longitude", 0.0),
+                  declare_parameter("map_origin.altitude", 0.0) };
 
   // Initialize subscribers and synchronizer here
   rmw_qos_profile_t custom_qos_profile = rmw_qos_profile_default;
@@ -24,13 +39,17 @@ OdomPublisher::OdomPublisher() : Node("odom_publisher_node")
                                                                                       imu_sub_, gps_sub_, pose_sub_);
   temp_sync_->registerCallback(std::bind(&OdomPublisher::topic_callback, this, std::placeholders::_1,
                                          std::placeholders::_2, std::placeholders::_3));
-
+  // Create a custom QoS profile
+  rclcpp::QoS profile(10);  // Set the buffer size to 10
+  profile.reliability(rmw_qos_reliability_policy_t::RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT);
+  profile.durability(rmw_qos_durability_policy_t::RMW_QOS_POLICY_DURABILITY_VOLATILE);
   // initialize publisher
   odom_publisher_ =
-      this->create_publisher<nav_msgs::msg::Odometry>("odom", 10);  // TODO: @sebastian change to best effort QoS
+      this->create_publisher<nav_msgs::msg::Odometry>("odom", profile);  // TODO: @sebastian change to best effort QoS
   tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(this);
 
-  timer_ = create_wall_timer(std::chrono::milliseconds(50), std::bind(&OdomPublisher::timer_callback, this));
+  timer_ = create_wall_timer(std::chrono::milliseconds(this->get_parameter("rate_millis").as_int()),
+                             std::bind(&OdomPublisher::timer_callback, this));
 
   // initialize buffer
   this->bufferSize = this->get_parameter("buffer_size").as_int();
@@ -67,12 +86,93 @@ void OdomPublisher::topic_callback(const sensor_msgs::msg::Imu::ConstSharedPtr& 
  */
 void OdomPublisher::timer_callback()
 {
-  RCLCPP_INFO(get_logger(), "buffer_size: [%d]", buffer.size());
   if (buffer.size() < bufferSize)
   {
+    RCLCPP_DEBUG(this->get_logger(), "Buffer size [%d] < [%d]", buffer.size(), bufferSize);
     return;
   }
-  // TODO: @sebatisan, finish impl here
+  auto odom = std::make_shared<nav_msgs::msg::Odometry>();
+
+  this->p_calcOdom(&this->buffer, odom);
+  this->publish_transform(odom);
+  this->odom_publisher_->publish(*odom);
+
+  RCLCPP_DEBUG(get_logger(), to_string(*odom));
+}
+
+void OdomPublisher::p_calcOdom(const std::vector<BufferData>* buffer, nav_msgs::msg::Odometry::SharedPtr odom)
+{
+  // Get the latest data from the buffer
+  const BufferData& latestData = buffer->back();
+
+  // Fill in the odometry message with the calculated average values
+  odom->header.stamp = buffer->back().imu_msg->header.stamp;
+  odom->header.frame_id = this->get_parameter("odom_frame").as_string();
+  odom->child_frame_id = this->get_parameter("base_link_frame").as_string();
+
+  // Calculate the average values
+  double avg_lat = 0.0;
+  double avg_long = 0.0;
+  double avg_alt = 0.0;
+
+  double avgVx = 0.0;
+  double avgVy = 0.0;
+  double avgVz = 0.0;
+
+  double angular_velocity_x = 0.0;
+  double angular_velocity_y = 0.0;
+  double angular_velocity_z = 0.0;
+
+  for (const BufferData& data : *buffer)
+  {
+    avg_lat += data.gps_msg->latitude;
+    avg_long += data.gps_msg->longitude;
+    avg_alt += data.gps_msg->altitude;
+
+    angular_velocity_x += data.imu_msg->angular_velocity.x;
+    angular_velocity_y += data.imu_msg->angular_velocity.y;
+    angular_velocity_z += data.imu_msg->angular_velocity.z;
+  }
+  // Calculate the average velocity
+  if (bufferSize > 1)
+  {
+    const BufferData& firstData = buffer->front();
+    const BufferData& lastData = buffer->back();
+    double dt = (lastData.pose_msg->header.stamp.nanosec - firstData.pose_msg->header.stamp.nanosec) /
+                1e9;  // Convert to seconds
+
+    avgVx = (lastData.pose_msg->pose.position.x - firstData.pose_msg->pose.position.x) / dt;
+    avgVy = (lastData.pose_msg->pose.position.y - firstData.pose_msg->pose.position.y) / dt;
+    avgVz = (lastData.pose_msg->pose.position.z - firstData.pose_msg->pose.position.z) / dt;
+  }
+
+  size_t bufferSize = buffer->size();
+  avg_lat /= bufferSize;
+  avg_long /= bufferSize;
+  avg_alt /= bufferSize;
+  angular_velocity_x /= bufferSize;
+  angular_velocity_y /= bufferSize;
+  angular_velocity_z /= bufferSize;
+
+  const GeographicLib::Geocentric& earth = GeographicLib::Geocentric::WGS84();
+  GeographicLib::LocalCartesian proj(map_origin_.latitude, map_origin_.longitude, map_origin_.altitude, earth);
+  proj.Forward(avg_lat, avg_long, avg_alt, odom->pose.pose.position.x, odom->pose.pose.position.y,
+               odom->pose.pose.position.z);
+
+  //    proj.Forward(latestData.gps_msg->latitude, latestData.gps_msg->longitude, latestData.gps_msg->altitude,
+  //    odom->pose.pose.position.x, odom->pose.pose.position.y,
+  //  odom->pose.pose.position.z);
+
+  odom->pose.pose.orientation = latestData.imu_msg->orientation;
+
+  // Fill in the velocity
+  odom->twist.twist.linear.x = avgVx;
+  odom->twist.twist.linear.y = avgVy;
+  odom->twist.twist.linear.z = avgVz;
+
+  odom->twist.twist.angular.x = angular_velocity_x;
+  odom->twist.twist.angular.y = angular_velocity_y;
+  odom->twist.twist.angular.z = angular_velocity_z;
 }
 
 void OdomPublisher::publish_transform(const nav_msgs::msg::Odometry::SharedPtr odom)
